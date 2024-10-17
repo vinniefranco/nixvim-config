7b263c622b4904523e0f9271e85d75243d257479
vinniefranco <vince@freshivore.net>
2024-10-17 12:25:19 -0500

    Fixing conform

--- config/conform.nix
+++ config/conform.nix
@@ -1,4 +1,4 @@
-{
+{ pkgs, ... } : {
   plugins = {
     conform-nvim = {
       enable = true;
@@ -10,43 +10,15 @@
         };
         formatters_by_ft = {
           javascript = [ "prettier" ];
-          "_" = [ "squeeze_blanks" "trim_whitespace" "trime_newlines" ];
+          "_" = [ "squeeze_blanks" "trim_whitespace" "trim_newlines" ];
         };
-        notify_on_error = true;
-        format_on_save = # Lua
-          ''
-            function(bufnr)
-              if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-                return
-              end
-
-              if slow_format_filetypes[vim.bo[bufnr].filetype] then
-                return
-              end
-
-              local function on_format(err)
-                if err and err:match("timeout$") then
-                  slow_format_filetypes[vim.bo[bufnr].filetype] = true
-                end
-              end
-
-              return { timeout_ms = 200, lsp_fallback = true }, on_format
-             end
-          '';
-        format_after_save = # Lua
-          ''
-            function(bufnr)
-              if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-                return
-              end
-
-              if not slow_format_filetypes[vim.bo[bufnr].filetype] then
-                return
-              end
-
-              return { lsp_fallback = true }
-            end
-          '';
+        notify_on_error = false;
+        format_on_save = { timeout = 500; };
+        formatters = {
+          squeeze_blanks = {
+            command = pkgs.lib.getExe' pkgs.coreutils "cat";
+          };
+        };
       };
     };
   };
