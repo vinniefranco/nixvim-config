d6da580476ba9def234752030fb34752e1b22cdf
vinniefranco <vince@freshivore.net>
2024-11-08 14:50:36 -0600

    Updating to latest

--- config/noice.nix
+++ config/noice.nix
@@ -1,88 +1,75 @@
 {
   plugins.noice = {
     enable = true;
-    lsp.override = {
-      "cmp.entry.get_documentation" = false;
-      "vim.lsp.util.convert_input_to_markdown_lines" = false;
-      "vim.lsp.util.stylize_markdown" = false;
-    };
-    commands = {
-      errors = {
-        filter = {
-          error = true;
-        };
-        filter_opts = {
-          reverse = true;
-        };
-        opts = {
-          enter = true;
-          format = "details";
-        };
-        view = "popup";
+    settings = {
+      lsp.override = {
+        "cmp.entry.get_documentation" = false;
+        "vim.lsp.util.convert_input_to_markdown_lines" = false;
+        "vim.lsp.util.stylize_markdown" = false;
       };
-      history = {
-        filter = {
-          any = [
-            {
-              event = "notify";
-            }
-            {
-              error = true;
-            }
-            {
-              warning = true;
-            }
-            {
-              event = "msg_show";
-              kind = [
-                ""
-              ];
-            }
-            {
-              event = "lsp";
-              kind = "message";
-            }
-          ];
+      commands = {
+        errors = {
+          filter = {
+            error = true;
+          };
+          filter_opts = {
+            reverse = true;
+          };
+          opts = {
+            enter = true;
+            format = "details";
+          };
+          view = "popup";
         };
-        opts = {
-          enter = true;
-          format = "details";
+        history = {
+          filter = {
+            any = [
+              { event = "notify"; }
+              { error = true; }
+              { warning = true; }
+              {
+                event = "msg_show";
+                kind = [ "" ];
+              }
+              {
+                event = "lsp";
+                kind = "message";
+              }
+            ];
+          };
+          opts = {
+            enter = true;
+            format = "details";
+          };
+          view = "split";
         };
-        view = "split";
+        last = {
+          filter = {
+            any = [
+              { event = "notify"; }
+              { error = true; }
+              { warning = true; }
+              {
+                event = "msg_show";
+                kind = [ "" ];
+              }
+              {
+                event = "lsp";
+                kind = "message";
+              }
+            ];
+          };
+          filter_opts = {
+            count = 1;
+          };
+          opts = {
+            enter = true;
+            format = "details";
+          };
+          view = "popup";
+        };
       };
-      last = {
-        filter = {
-          any = [
-            {
-              event = "notify";
-            }
-            {
-              error = true;
-            }
-            {
-              warning = true;
-            }
-            {
-              event = "msg_show";
-              kind = [
-                ""
-              ];
-            }
-            {
-              event = "lsp";
-              kind = "message";
-            }
-          ];
-        };
-        filter_opts = {
-          count = 1;
-        };
-        opts = {
-          enter = true;
-          format = "details";
-        };
-        view = "popup";
-      };
+
     };
   };
 }
