1dbb2a50966dc13230ff536c76aa30ad5c88b56b
vinniefranco <vince@freshivore.net>
2024-12-09 10:06:16 -0600

    Working up noice

--- config/noice.nix
+++ config/noice.nix
@@ -2,10 +2,12 @@
   plugins.noice = {
     enable = true;
     settings = {
-      lsp.override = {
-        "cmp.entry.get_documentation" = false;
-        "vim.lsp.util.convert_input_to_markdown_lines" = false;
-        "vim.lsp.util.stylize_markdown" = false;
+      lsp = {
+        signature.enabled = true;
+        progress.enabled = false;
+        signature.view = "virtualtext";
+        hover.view = "virtualtext";
+        documentation.view = "virtualtext";
       };
       commands = {
         errors = {
